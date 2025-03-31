1bef36cf6d30b25c1f54665b5ebcc2e1c67f1fd4
Ethan Arias <ethanarias11@gmail.com>
2025-03-31 19:15:19 -0400

    Added the loop to collect couse data inn Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
 #include "Course.h"
 
 int main()
 {
+	std::vector<Course> cList(10);
+	int size = 0;
 
+	std::cout << "How many cources have you taken?" << std::endl;
+	std::cin >> size;
+	if (size < 0) {
+		size *= -1;
+	}
+
+	for (int i = 0; i < size; ++i) {
+		std::string courseName = "";
+		int courseCredits = 0;
+		std::string courseGrade = "";
+		cList.push_back(Course());
+
+		std::cout << "\tWhat is the name of the " << i+1<< "course?" << std::endl;
+		std::cin >> courseName;
+		std::cout << "\tWhat is the number of credits for the " << i + 1 << "course?" << std::endl;
+		std::cin >> courseCredits;
+		std::cout << "\tWhat is the your grade for the " << i + 1 << "course?" << std::endl;
+		std::cin >> courseGrade;
+
+		cList[cList.size()].setname(courseName);
+		cList[cList.size()].setcredits(courseCredits);
+		cList[cList.size()].setGrade(courseGrade);
+	}
 }
